91b5dd03f936558fcb2c7a85518b7ec344581dc9
Barpotroszek <77773157+Barpotroszek@users.noreply.github.com>
2022-10-11 16:30:03 +0200

    fix: nie bralo tego co w nawiasach

--- parser.js
+++ parser.js
@@ -1,23 +1,30 @@
 const fs = require("fs");
 
-let data="";
+let data = "";
 
-const rs = fs.createReadStream("data.txt", {encoding: "utf-8"}),
-    ws = fs.createWriteStream("output.json", {encoding: "utf-8"});
+const rs = fs.createReadStream("data.txt", { encoding: "utf-8" }),
+  ws = fs.createWriteStream("output.json", { encoding: "utf-8" });
 
-rs.on("data", (d)=> data+=d);
-rs.on("end", ()=>{;
-    let arr=[], words = data.split(";");
-    console.log("words length:", words.length)
-    words.forEach(word => {
-        const regex =  /(?<eng>['\w\s-]+) - (?<pl>[\s\S]+)/g;
-        let a = regex.exec(word);
-        if(!a) return
-        // console.log(a.groups)
-        let {eng, pl} = a.groups;
-        arr.push({eng, pl});
-    });
-    // console.log(arr)
-    ws.write(JSON.stringify(arr), "utf-8", ()=>ws.close());
-    rs.close();
-})
+rs.on("data", (d) => (data += d));
+rs.on("end", () => {
+  let arr = [],
+    words = data.split(";"),
+    i = 0;
+  console.log("words length:", words.length);
+  words.forEach((word) => {
+    i++;
+    const regex = /(?<eng>['\w\s-\(\)]+) - (?<pl>[\s\S\(\)]+)/g;
+    let a = regex.exec(word);
+    if (!a) {
+        console.log(i, word, a)
+        return;
+    }
+    // console.log(a.groups)
+    let { eng, pl } = a.groups;
+    arr.push({ eng, pl });
+  });
+  console.log(arr.length, "i:", i);
+  // console.log(arr)
+  ws.write(JSON.stringify(arr), "utf-8", () => ws.close());
+  rs.close();
+});
